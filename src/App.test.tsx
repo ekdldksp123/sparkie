2e460a37347ad56f60bb8b0ca0841c441ff43aea
mynamesunpower <mynamesunpower@gmail.com>
2021-12-14 23:51:43 +0900

    fix: test test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('test', () => {
+  let a = 1;
+  expect(a + 1).toBe(2);
 });
