f8cfa5ae82046d14d78103a0d582304c6c8438c6
mynamesunpower <mynamesunpower@gmail.com>
2021-12-22 23:19:49 +0900

    style: Layout 수정중

---
+++ src/components/views/QnA/InnerBox.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const StyledBox = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+`;
+
+const UpperBox = styled.div`
+  box-sizing: border-box;
+  padding: 2rem 1.5rem 2rem 1.5rem;
+  display: flex;
+  align-items: center;
+`;
+
+const LeftBox = styled.div`
+  flex: 1;
+  h1 {
+  }
+  span {
+    font-size: 0.8rem;
+    color: darkgray;
+  }
+  div {
+  }
+`;
+const RightBox = styled.div``;
+
+const BottomBox = styled.div`
+  padding: 1rem;
+  p {
+  }
+`;
+
+const InnerBox = (props: any) => {
+  return (
+    <StyledBox>
+      {/* 상단 UI : flex */}
+      <UpperBox>
+        {/* 중간에 가도록. */}
+        <LeftBox>
+          <div>
+            <h1>오늘 중고차 구매했는데요..</h1>
+            <span>3분 전</span>
+          </div>
+        </LeftBox>
+        <RightBox>써니킴</RightBox>
+      </UpperBox>
+      {/*  하단 UI */}
+      <BottomBox>
+        <p>뻥입니다. 하하하하하하</p>
+      </BottomBox>
+    </StyledBox>
+  );
+};
+
+export default InnerBox;
--- src/components/views/QnA/LeftLayout.tsx
+++ src/components/views/QnA/LeftLayout.tsx
@@ -4,11 +4,12 @@ import styled from '@emotion/styled';
 const StyledLeftBox = styled.div`
   display: flex;
   justify-content: flex-end;
-  width: 40%;
+  width: 25%;
+  border: 1px solid blue;
 `;
 
 const CategoryBox = styled.div`
-  margin: 1rem 0 1rem 1rem;
+  margin: 1rem 0.3rem 1rem 1rem;
   border-radius: 5rem;
   width: 10%;
   display: flex;
@@ -17,13 +18,28 @@ const CategoryBox = styled.div`
 `;
 
 const StyledCategory = styled.div`
-  border-radius: 50%;
+  position: relative;
   width: 100%;
-  height: 8%;
-  margin-bottom: 1rem;
-  background-color: bisque;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
-    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  height: 9%;
+  //height: 150px;
+  margin: 0.5rem 0;
+  background: bisque;
+  border-radius: 50% / 10%;
+  color: white;
+  text-align: center;
+  text-indent: 0.1em;
+  box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+    rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 10%;
+    bottom: 10%;
+    right: -5%;
+    left: -5%;
+    background: inherit;
+    border-radius: 5% / 50%;
+  }
 `;
 
 const StyledBoard = styled.div`
