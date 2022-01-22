67ef011c7b6b55dbc704a3426be73f5132ce3391
ekdldksp123 <ekdldksp123@naver.com>
2022-01-23 05:03:24 +0900

    [Add] add updating comments data on server module but not working

--- src/components/views/Community.tsx
+++ src/components/views/Community.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import Calendar from './community/Calendar';
-import {DateProps} from '../../types/components/views/DateProps';
+import { DateProps } from '../../types/components/views/DateProps';
 import Board from "./community/Board";
 import { Post, PostData, Comment } from "../../types/components/views/BoardProps";
 import { formatDateToString } from "../../lib/common/DateUtils";
