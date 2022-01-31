f8cc7d457230563b3b798a2f8dd7487369223818
ekdldksp123 <ekdldksp123@naver.com>
2022-02-01 02:05:25 +0900

    [Add] add intro page and apply animation to text svg

--- src/components/views/Intro.tsx
+++ src/components/views/Intro.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+/** @jsxImportSource @emotion/react */
+import { css, keyframes } from "@emotion/react";
+import styled from '@emotion/styled';
+import React, { useEffect, useState, useRef } from 'react';
+import tesla from '../../assets/tesla.gif';
+import AnimatedText from "./intro/JustDoIt";
 
 const Intro = () => {
   return (
-    <div>
-      <h1>인트로</h1>
-      <p>
-        <Link to={'/main'}>메인 ㄱㄱ</Link>
-      </p>
-    </div>
+    <>
+      <Gif>
+        <img src={tesla} css={car} alt="init"/>
+      </Gif>
+      <Container>
+        <AnimatedText/>
+      </Container>
+    </>
   );
-};
+}
 
 export default Intro;
+
+const Gif = styled.div`
+  width: 100vw;
+  height: 70vh;
+`;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const car = css`
+  width: 100%;
+  height: 100%;
+  object-fit: cover !important;
+`
